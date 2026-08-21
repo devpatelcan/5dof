1dcbf33dd0859b65e450a82329d8ec56b12a9855
devpatelcan <devpatelcan100@gmail.com>
2026-08-21 19:19:46 -0400

    Remove all files

--- urdf/save_xml.py
+++
@@ -1,8 +0,0 @@
-import mujoco
-
-# Use the absolute path so Python finds it regardless of where the terminal is open
-urdf_path = r"C:\Dev's Stuff\Personal Projects\5DOF\Robotic_Sim\New_urdf\urdf\New_urdf.urdf"
-
-# Read the URDF and save it as a native MuJoCo XML
-model = mujoco.MjModel.from_xml_path(urdf_path)
-mujoco.mj_saveLastXML("Robotic_Sim.xml", model)
